5550745fcd8a3bb71790a21523c79004e3105950
zach <franc230@msu.edu>
2020-06-04 11:53:34 -0400

    Creating blog, database and oral history files

---
+++ blog.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="A digital scrapbook with multiple cultural heritage projects">
+  <meta name="author" content="Zach Francis & Kayla Kubehl">
+
+  <title>VodkaWagon</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="css/clean-blog.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">VodkaWagon</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="post.html">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Oral Histories</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="map.html">Map</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Database</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contribute.html">Contribute</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>Welcome to VodkaWagon</h1>
+            <span class="subheading">An Online Scrapbook</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+<!-- Portfolio Section for project Pages -->
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Scrapbook Pages</h2>
+            <h3 class="section-subheading text-muted">Each "page" is a project or description related to our heritage</h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="portfolio-item">
+                    <a class="portfolio-link" href="about.html">
+                        <img class="img-fluid" src="img/home-bg.jpg" alt=""/></a>
+                    <div class="portfolio-caption">
+                        <div class="portfolio-caption-heading">Blog</div>
+                        <div class="portfolio-caption-subheading text-muted">Updated monthly</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+              <div class="portfolio-item">
+                  <a class="portfolio-link" href="about.html">
+                      <img class="img-fluid" src="img/post-bg.jpg" alt=""/></a>
+                  <div class="portfolio-caption">
+                      <div class="portfolio-caption-heading">Oral Histories</div>
+                      <div class="portfolio-caption-subheading text-muted">Friends and family</div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+              <div class="portfolio-item">
+                  <a class="portfolio-link" href="map.html">
+                      <img class="img-fluid" src="img/about-bg.jpg" alt=""/></a>
+                  <div class="portfolio-caption">
+                      <div class="portfolio-caption-heading">The Map</div>
+                      <div class="portfolio-caption-subheading text-muted">Where have we been?</div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
+              <div class="portfolio-item">
+                  <a class="portfolio-link" href="about.html">
+                      <img class="img-fluid" src="img/post-sample-image.jpg" alt=""/></a>
+                  <div class="portfolio-caption">
+                      <div class="portfolio-caption-heading">Database</div>
+                      <div class="portfolio-caption-subheading text-muted">All of our records</div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4 mb-sm-0">
+              <div class="portfolio-item">
+                  <a class="portfolio-link" href="contribute.html">
+                      <img class="img-fluid" src="img/contact-bg.jpg" alt=""/></a>
+                  <div class="portfolio-caption">
+                      <div class="portfolio-caption-heading">Contribute</div>
+                      <div class="portfolio-caption-subheading text-muted">Speak your piece!</div>
+                  </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+              <div class="portfolio-item">
+                  <a class="portfolio-link" href="about.html">
+                      <img class="img-fluid" src="img/about-speedwagon2.png" alt=""/></a>
+                  <div class="portfolio-caption">
+                      <div class="portfolio-caption-heading">About</div>
+                      <div class="portfolio-caption-subheading text-muted">About the developers</div>
+                  </div>
+              </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+<hr>
+
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item">
+              <a href="https://twitter.com/RedFeather0">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="facebook.com">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://github.com/0Redfeather/vodkawagon">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+          <p class="copyright text-muted">Copyright &copy; VodkaWagon.dev 2020 <br> All rights Reserved</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
